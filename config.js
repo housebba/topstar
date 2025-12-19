402fabe6755c3c9a26e14060f998d479c86cce33
housebba <housebba@users.noreply.github.com>
2025-12-19 06:18:33 +0000

    deploy: 57c946c2692d20ec1b0b942e0e08c76363d261ed

--- config.js
+++
@@ -1,7 +0,0 @@
-const CONFIG = {
-    NODES: [
-        { id: 'node-01', name: 'Validator Node 01', url: 'http://168.107.52.88:26657' },
-        { id: 'node-02', name: 'Validator Node 02', url: 'http://168.107.8.4:26657' },
-        { id: 'node-03', name: 'Validator Node 03', url: 'http://158.179.168.231:26657' }
-    ]
-};
